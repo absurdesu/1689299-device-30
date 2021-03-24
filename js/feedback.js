db0e41fe879261f6c5b4ba80f85bd6144b245a62
absurdesu <homo.ridens.dclxvi@gmail.com>
2021-03-24 12:43:51 +0300

    Оживляем интерактивные элементы

--- js/feedback.js
+++ js/feedback.js
@@ -1,16 +1,30 @@
 const feedbackLink = document.querySelector(".feedback-link");
 const feedbackPopup = document.querySelector(".modal-feedback");
 const feedbackClose = feedbackPopup.querySelector(".modal-close");
+const feedbackForm = feedbackPopup.querySelector(".feedback-form");
+const feedbackName = feedbackPopup.querySelector("[name=name]");
+const feedbackEmail = feedbackPopup.querySelector("[name=email]");
+const feedbackMessage = feedbackPopup.querySelector("[name=message]");
 
 feedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.add("modal-show");
-  name.focus();
+  feedbackName.focus();
 });
 
 feedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.remove("modal-show");
+  feedbackPopup.classList.remove("modal-error");
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackMessage.value) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove("modal-error");
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add("modal-error");
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -18,6 +32,7 @@ window.addEventListener("keydown", function (evt) {
     if (feedbackPopup.classList.contains("modal-show")) {
       evt.preventDefault();
       feedbackPopup.classList.remove("modal-show");
+      feedbackPopup.classList.remove("modal-error");
     }
   }
 });
