8c13c73c41d3f20dcd4be8a06ccdf3b981b3b08a
absurdesu <homo.ridens.dclxvi@gmail.com>
2021-03-24 15:28:39 +0300

    Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const mapLink = document.querySelector(".map-link");
+const mapPopup = document.querySelector(".modal-map");
+const mapClose = mapPopup.querySelector(".modal-close");
 const feedbackLink = document.querySelector(".feedback-link");
 const feedbackPopup = document.querySelector(".modal-feedback");
 const feedbackClose = feedbackPopup.querySelector(".modal-close");
@@ -5,6 +8,25 @@ const feedbackForm = feedbackPopup.querySelector(".feedback-form");
 const feedbackName = feedbackPopup.querySelector("[name=name]");
 const feedbackEmail = feedbackPopup.querySelector("[name=email]");
 const feedbackMessage = feedbackPopup.querySelector("[name=message]");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
 
 feedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
